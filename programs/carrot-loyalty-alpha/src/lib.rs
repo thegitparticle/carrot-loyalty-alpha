cbec908d405be7a6a650cc383a070ca5620af5e8
Set <12975157+thegitparticle@users.noreply.github.com>
2022-08-13 20:19:43 +0530

    program ID generated and added

--- programs/carrot-loyalty-alpha/src/lib.rs
+++ programs/carrot-loyalty-alpha/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("5jtwmP1bNjvEAu23qerh2WCzpULsqZJuGX5cFATbZBHS");
 
 #[program]
 pub mod carrot_loyalty_alpha {
