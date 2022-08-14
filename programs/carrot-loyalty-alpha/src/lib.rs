e13c1bfc297ce7e193f0a7818eb8b14c705f391b
Set <12975157+thegitparticle@users.noreply.github.com>
2022-08-14 20:44:13 +0530

    create loyalty by and signed by a consumer for each brand

--- programs/carrot-loyalty-alpha/src/lib.rs
+++ programs/carrot-loyalty-alpha/src/lib.rs
@@ -21,6 +21,28 @@ pub mod carrot_loyalty_alpha {
         brand.brand_address = *brand_address.key;
         Ok(())
     }
+
+    pub fn create_loyalty(
+        ctx: Context<CreateNewLoyalty>,
+        brand_address: Pubkey,
+        brand_name: String,
+        loyalty_score: u64,
+        loyalty_level: u64,
+    ) -> ProgramResult {
+        let loyalty = &mut ctx.accounts.loyalty;
+        let authority_consumer = &mut ctx.accounts.authority_consumer;
+
+        // let brand_address = &mut ctx.accounts.brand_address;
+        // let consumer_address = &mut ctx.accounts.consumer_address;
+
+        loyalty.brand_address = brand_address;
+        loyalty.consumer_address = *authority_consumer.key;
+        loyalty.brand_name = brand_name;
+        loyalty.loyalty_score = loyalty_score;
+        loyalty.loyalty_level = loyalty_level;
+
+        Ok(())
+    }
 }
 
 #[error_code]
@@ -28,6 +50,15 @@ pub enum ErrorCode {
     #[msg("The provided brand name should be 75 characters long maximum.")]
     BrandNameTooLong,
 }
+
+// Add some useful constants for sizing propeties.
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
+const MAX_BRAND_NAME_LENGTH: usize = 75 * 4; // 75 chars max.
+const MAX_LOGO_LINK_LENGTH: usize = 75 * 4; // 75 chars max.
+const LOYALTY_SCORE_LENGTH: usize = 8;
+const LOYALTY_LEVEL_LENGTH: usize = 8;
 
 #[derive(Accounts)]
 pub struct EnrollNewBrand<'info> {
@@ -46,17 +77,38 @@ pub struct Brand {
     pub logo_link: String,
 }
 
-// 2. Add some useful constants for sizing propeties.
-const DISCRIMINATOR_LENGTH: usize = 8;
-const PUBLIC_KEY_LENGTH: usize = 32;
-const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
-const MAX_BRAND_NAME_LENGTH: usize = 75 * 4; // 75 chars max.
-const MAX_LOGO_LINK_LENGTH: usize = 75 * 4; // 75 chars max.
-
-// 3. implementation block for size calculation
+// implementation block for size calculation
 impl Brand {
     const LEN: usize = DISCRIMINATOR_LENGTH
         + PUBLIC_KEY_LENGTH // Brand Address.
         + STRING_LENGTH_PREFIX + MAX_BRAND_NAME_LENGTH // brand name string
         + STRING_LENGTH_PREFIX + MAX_LOGO_LINK_LENGTH;
+}
+
+#[derive(Accounts)]
+pub struct CreateNewLoyalty<'info> {
+    #[account(init, payer = authority_consumer, space = Loyalty::LEN)]
+    pub loyalty: Account<'info, Loyalty>,
+    #[account(mut)]
+    pub authority_consumer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Loyalty {
+    pub consumer_address: Pubkey,
+    pub brand_address: Pubkey,
+    pub brand_name: String,
+    pub loyalty_score: u64,
+    pub loyalty_level: u64,
+}
+
+// implementation block for size calculation
+impl Loyalty {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // Brand address
+        + PUBLIC_KEY_LENGTH // Consumer address.
+        + STRING_LENGTH_PREFIX + MAX_BRAND_NAME_LENGTH  // brand name string
+        + LOYALTY_SCORE_LENGTH // loyalty score.
+        + LOYALTY_LEVEL_LENGTH; // loyalty level.
 }
--- tests/carrot-loyalty-alpha.js
+++ tests/carrot-loyalty-alpha.js
@@ -8,9 +8,10 @@ describe("carrot-loyalty-alpha", () => {
 
 	const program = anchor.workspace.CarrotLoyaltyAlpha;
 
+	const brand = anchor.web3.Keypair.generate();
+
 	it("creating a new brand account", async () => {
 		// generate a keypair to act as new's brand account
-		const brand = anchor.web3.Keypair.generate();
 
 		let brand_logo_sample =
 			"https://assets.supremenewyork.com/assets/logo-supreme-7fbf1f6597b0a6a686e03c82c29b8e7d.png";
@@ -26,12 +27,60 @@ describe("carrot-loyalty-alpha", () => {
 
 		const brandAccount = await program.account.brand.fetch(brand.publicKey);
 
-		console.log(brandAccount);
+		// console.log(brandAccount);
 
 		assert.equal(
 			brandAccount.brandAddress.toBase58(),
 			program.provider.wallet.publicKey.toBase58()
 		);
 		assert.equal(brandAccount.brandName, "supreme");
+	});
+
+	it("creating a new loyalty account", async () => {
+		// generate a keypair to act as new's consumer account
+		const consumer = anchor.web3.Keypair.generate();
+		const loyalty = anchor.web3.Keypair.generate();
+
+		const signature = await program.provider.connection.requestAirdrop(
+			consumer.publicKey,
+			1000000000
+		);
+		await program.provider.connection.confirmTransaction(signature);
+
+		console.log("loyalty and bump found");
+
+		let score = new anchor.BN(711);
+		let level = new anchor.BN(3);
+
+		console.log("score and level converted to BigNumber");
+
+		await program.rpc.createLoyalty(
+			brand.publicKey,
+			"supreme",
+			score,
+			level,
+			{
+				accounts: {
+					loyalty: loyalty.publicKey,
+					authorityConsumer: consumer.publicKey,
+					systemProgram: anchor.web3.SystemProgram.programId,
+				},
+				signers: [consumer, loyalty],
+			}
+		);
+
+		console.log("rpc call sent out");
+
+		const loyaltyAccount = await program.account.loyalty.fetch(
+			loyalty.publicKey
+		);
+
+		console.log(loyaltyAccount);
+
+		assert.equal(
+			loyaltyAccount.brandAddress.toBase58(),
+			brand.publicKey.toBase58()
+		);
+		assert.equal(loyaltyAccount.brandName, "supreme");
 	});
 });
