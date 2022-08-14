bf809da5dd37dcffe6dc799e574115d90a4400b1
Set <12975157+thegitparticle@users.noreply.github.com>
2022-08-15 03:59:19 +0530

    mint nfts program instruction added - tests setup

--- programs/carrot-loyalty-alpha/src/lib.rs
+++ programs/carrot-loyalty-alpha/src/lib.rs
@@ -1,4 +1,8 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::program::invoke;
+use anchor_spl::token;
+use anchor_spl::token::{MintTo, Token};
+use mpl_token_metadata::instruction::{create_master_edition_v3, create_metadata_accounts_v2};
 
 declare_id!("5jtwmP1bNjvEAu23qerh2WCzpULsqZJuGX5cFATbZBHS");
 
@@ -28,6 +32,7 @@ pub mod carrot_loyalty_alpha {
         brand_name: String,
         loyalty_score: u64,
         loyalty_level: u64,
+        // brand_details: Brand,
     ) -> ProgramResult {
         let loyalty = &mut ctx.accounts.loyalty;
         let consumer_address = &mut ctx.accounts.consumer_address;
@@ -37,6 +42,26 @@ pub mod carrot_loyalty_alpha {
         loyalty.brand_name = brand_name;
         loyalty.loyalty_score = loyalty_score;
         loyalty.loyalty_level = loyalty_level;
+
+        // let mintNftContext: MintNFT = {};
+
+        // mint_nft(
+        //     { accounts: {
+        //         mintAuthority.publicKey,
+        //         mint: mintKey.publicKey,
+        //         tokenAccount: NftTokenAccount,
+        //         tokenProgram: TOKEN_PROGRAM_ID,
+        //         metadata: metadataAddress,
+        //         tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+        //         payer: wallet.publicKey,
+        //         systemProgram: SystemProgram.programId,
+        //         rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        //         masterEdition: masterEdition,
+        //       } },
+        //     *consumer_address.key,
+        //     brand_details.logo_link,
+        //     brand_name,
+        // );
 
         Ok(())
     }
@@ -73,6 +98,100 @@ pub mod carrot_loyalty_alpha {
             loyalty.loyalty_level = 1
         }
 
+        Ok(())
+    }
+
+    pub fn mint_nft(
+        ctx: Context<MintNFT>,
+        creator_key: Pubkey,
+        uri: String,
+        title: String,
+    ) -> Result<()> {
+        msg!("Initializing Mint NFT");
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info(),
+        };
+        msg!("CPI Accounts Assigned");
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        msg!("CPI Program Assigned");
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        msg!("CPI Context Assigned");
+        token::mint_to(cpi_ctx, 1)?;
+        msg!("Token Minted !!!");
+        let account_info = vec![
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.token_metadata_program.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+        msg!("Account Info Assigned");
+        let creator = vec![
+            mpl_token_metadata::state::Creator {
+                address: creator_key,
+                verified: false,
+                share: 100,
+            },
+            mpl_token_metadata::state::Creator {
+                address: ctx.accounts.mint_authority.key(),
+                verified: false,
+                share: 0,
+            },
+        ];
+        msg!("Creator Assigned");
+        let symbol = std::string::ToString::to_string("symb");
+        invoke(
+            &create_metadata_accounts_v2(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.mint.key(),
+                ctx.accounts.mint_authority.key(),
+                ctx.accounts.payer.key(),
+                ctx.accounts.payer.key(),
+                title,
+                symbol,
+                uri,
+                Some(creator),
+                1,
+                true,
+                false,
+                None,
+                None,
+            ),
+            account_info.as_slice(),
+        )?;
+        msg!("Metadata Account Created !!!");
+        let master_edition_infos = vec![
+            ctx.accounts.master_edition.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.token_metadata_program.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+        msg!("Master Edition Account Infos Assigned");
+        invoke(
+            &create_master_edition_v3(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.master_edition.key(),
+                ctx.accounts.mint.key(),
+                ctx.accounts.payer.key(),
+                ctx.accounts.mint_authority.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.payer.key(),
+                Some(0),
+            ),
+            master_edition_infos.as_slice(),
+        )?;
+        msg!("Master Edition Nft Minted !!!");
         Ok(())
     }
 }
@@ -150,4 +269,32 @@ impl Loyalty {
         + STRING_LENGTH_PREFIX + MAX_BRAND_NAME_LENGTH  // brand name string
         + LOYALTY_SCORE_LENGTH // loyalty score.
         + LOYALTY_LEVEL_LENGTH; // loyalty level.
+}
+
+#[derive(Accounts)]
+pub struct MintNFT<'info> {
+    #[account(mut)]
+    pub mint_authority: Signer<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub mint: UncheckedAccount<'info>,
+    // #[account(mut)]
+    pub token_program: Program<'info, Token>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub token_account: UncheckedAccount<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub token_metadata_program: UncheckedAccount<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub payer: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub rent: AccountInfo<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub master_edition: UncheckedAccount<'info>,
 }
--- tests/carrot-loyalty-alpha.js
+++ tests/carrot-loyalty-alpha.js
@@ -1,6 +1,14 @@
 const anchor = require("@project-serum/anchor");
 const assert = require("assert");
 const bs58 = require("bs58");
+// import {
+// 	TOKEN_PROGRAM_ID,
+// 	createAssociatedTokenAccountInstruction,
+// 	getAssociatedTokenAddress,
+// 	createInitializeMintInstruction,
+// 	MINT_SIZE,
+// } from "@solana/spl-token"; // IGNORE THESE ERRORS IF ANY
+const splToken = require("@solana/spl-token");
 
 describe("carrot-loyalty-alpha", () => {
 	// Configure the client to use the local cluster.
@@ -11,7 +19,11 @@ describe("carrot-loyalty-alpha", () => {
 	const brand = anchor.web3.Keypair.generate();
 	const consumer = anchor.web3.Keypair.generate();
 
+	console.log(consumer.publicKey, consumer.privateKey);
+
 	const loyalty = anchor.web3.Keypair.generate();
+
+	/*
 
 	it("creating a new brand account", async () => {
 		// generate a keypair to act as new's brand account
@@ -75,14 +87,14 @@ describe("carrot-loyalty-alpha", () => {
 			brand.publicKey.toBase58()
 		);
 		assert.equal(loyaltyAccount.brandName, "supreme");
-		console.log(loyaltyAccount.loyaltyScore.toNumber());
-		console.log(loyaltyAccount.loyaltyLevel.toNumber());
+		// console.log(loyaltyAccount.loyaltyScore.toNumber());
+		// console.log(loyaltyAccount.loyaltyLevel.toNumber());
 	});
 
 	it("can fetch all loyalty accounts", async () => {
 		const allLoyaltyAccounts = await program.account.loyalty.all();
 		// assert.equal(allLoyaltyAccounts.length, 3);
-		console.log(allLoyaltyAccounts[0]);
+		// console.log(allLoyaltyAccounts[0]);
 
 		const loyaltyAccountToEdit = allLoyaltyAccounts[0];
 
@@ -96,12 +108,128 @@ describe("carrot-loyalty-alpha", () => {
 			signers: [consumer],
 		});
 
-		console.log("rpc updateloyalty sent");
+		// console.log("rpc updateloyalty sent");
 
 		const allLoyaltyAccountsAgain = await program.account.loyalty.all();
 		// assert.equal(allLoyaltyAccounts.length, 3);
-		console.log(allLoyaltyAccountsAgain[0]);
-		console.log(allLoyaltyAccountsAgain[0].account.loyaltyScore.toNumber());
-		console.log(allLoyaltyAccountsAgain[0].account.loyaltyLevel.toNumber());
+		// console.log(allLoyaltyAccountsAgain[0]);
+		// console.log(allLoyaltyAccountsAgain[0].account.loyaltyScore.toNumber());
+		// console.log(allLoyaltyAccountsAgain[0].account.loyaltyLevel.toNumber());
+	});
+
+	*/
+
+	it("mint the loyalty nft", async () => {
+		const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
+			"metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+		);
+		const lamports =
+			await program.provider.connection.getMinimumBalanceForRentExemption(
+				splToken.MINT_SIZE
+			);
+
+		const signature = await program.provider.connection.requestAirdrop(
+			consumer.publicKey,
+			1000000000
+		);
+		await program.provider.connection.confirmTransaction(signature);
+
+		const getMetadata = async (mint) => {
+			return (
+				await anchor.web3.PublicKey.findProgramAddress(
+					[
+						Buffer.from("metadata"),
+						TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+						mint.toBuffer(),
+					],
+					TOKEN_METADATA_PROGRAM_ID
+				)
+			)[0];
+		};
+
+		const getMasterEdition = async (mint) => {
+			return (
+				await anchor.web3.PublicKey.findProgramAddress(
+					[
+						Buffer.from("metadata"),
+						TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+						mint.toBuffer(),
+						Buffer.from("edition"),
+					],
+					TOKEN_METADATA_PROGRAM_ID
+				)
+			)[0];
+		};
+
+		const mintKey = anchor.web3.Keypair.generate();
+		const NftTokenAccount = await splToken.getAssociatedTokenAddress(
+			mintKey.publicKey,
+			consumer.publicKey
+		);
+		console.log("NFT Account: ", NftTokenAccount.toBase58());
+
+		const mint_tx = new anchor.web3.Transaction().add(
+			anchor.web3.SystemProgram.createAccount({
+				fromPubkey: consumer.publicKey,
+				newAccountPubkey: mintKey.publicKey,
+				space: splToken.MINT_SIZE,
+				programId: splToken.TOKEN_PROGRAM_ID,
+				lamports,
+			}),
+			splToken.createInitializeMintInstruction(
+				mintKey.publicKey,
+				0,
+				consumer.publicKey,
+				consumer.publicKey
+			),
+			splToken.createAssociatedTokenAccountInstruction(
+				consumer.publicKey,
+				NftTokenAccount,
+				consumer.publicKey,
+				mintKey.publicKey
+			)
+		);
+
+		console.log("mint txn setup");
+
+		const res = await program.provider.sendAndConfirm(mint_tx, [
+			mintKey,
+			consumer,
+		]);
+		console.log(
+			await program.provider.connection.getParsedAccountInfo(
+				mintKey.publicKey
+			)
+		);
+
+		console.log("Account: ", res);
+		console.log("Mint key: ", mintKey.publicKey.toString());
+		console.log("User: ", consumer.publicKey.toString());
+
+		const metadataAddress = await getMetadata(mintKey.publicKey);
+		const masterEdition = await getMasterEdition(mintKey.publicKey);
+
+		console.log("Metadata address: ", metadataAddress.toBase58());
+		console.log("MasterEdition: ", masterEdition.toBase58());
+
+		await program.rpc.mintNft(
+			mintKey.publicKey,
+			"https://arweave.net/y5e5DJsiwH0s_ayfMwYk-SnrZtVZzHLQDSTZ5dNRUHA",
+			"Supreme NFT #1",
+			{
+				accounts: {
+					mintAuthority: consumer.publicKey,
+					mint: mintKey.publicKey,
+					tokenAccount: NftTokenAccount,
+					tokenProgram: splToken.TOKEN_PROGRAM_ID,
+					metadata: metadataAddress,
+					tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+					payer: consumer.publicKey,
+					systemProgram: SystemProgram.programId,
+					rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+					masterEdition: masterEdition,
+				},
+			}
+		);
 	});
 });
